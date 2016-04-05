453de4d71b3273f5884ec0d278bb848dc2a611e7
Nunie123 <ed.a.nunes@gmail.com>
2016-04-05 19:28:36 -0400

    Added user and admin functionality (Ch.9)

--- cantrip/db/schema.rb
+++ cantrip/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404204019) do
+ActiveRecord::Schema.define(version: 20160405231554) do
 
   create_table "users", force: :cascade do |t|
     t.string   "password",        limit: 255
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160404204019) do
     t.string   "password_digest", limit: 255
     t.string   "name",            limit: 255
     t.string   "remember_digest", limit: 255
+    t.boolean  "admin"
   end
 
 end
