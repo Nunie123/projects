dcd7fa73b2e07eaffd3ad10b6786f63345c48c28
Nunie123 <ed.a.nunes@gmail.com>
2016-04-26 19:30:34 -0400

    Initial Commit

--- cantrip/db/schema.rb
+++ cantrip/db/schema.rb
@@ -11,17 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405231554) do
+ActiveRecord::Schema.define(version: 20160406200124) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "password",        limit: 255
-    t.string   "email",           limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "password_digest", limit: 255
-    t.string   "name",            limit: 255
-    t.string   "remember_digest", limit: 255
+    t.string   "password",          limit: 255
+    t.string   "email",             limit: 255
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "password_digest",   limit: 255
+    t.string   "name",              limit: 255
+    t.string   "remember_digest",   limit: 255
     t.boolean  "admin"
+    t.string   "activation_digest", limit: 255
+    t.boolean  "activated",                     default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
 end
